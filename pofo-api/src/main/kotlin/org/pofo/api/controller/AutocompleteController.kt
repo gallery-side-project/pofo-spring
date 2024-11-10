705a539f5022c9dbc1238e1c9c8afaeba2bc1541
mclub4 <mclub4@kookmin.ac.kr>
2024-11-10 12:55:34 +0900

    fix: wildcard annotation 삭제

--- pofo-api/src/main/kotlin/org/pofo/api/controller/AutocompleteController.kt
+++ pofo-api/src/main/kotlin/org/pofo/api/controller/AutocompleteController.kt
@@ -3,7 +3,12 @@ package org.pofo.api.controller
 import com.pofo.elasticsearch.document.TechStackAutoComplete
 import org.pofo.api.service.OpenSearchService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RequestMapping("/tech-stack")
