92434988f24339afc8d4e9b16d71eb172d94641b
HongSukju <ehensnfl@gmail.com>
2024-12-16 11:05:44 +0900

    fix: do not mapping redis config

--- pofo-domain/src/main/kotlin/org/pofo/domain/redis/config/RedisConfig.kt
+++ pofo-domain/src/main/kotlin/org/pofo/domain/redis/config/RedisConfig.kt
@@ -1,21 +1,8 @@
 package org.pofo.domain.redis.config
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @Configuration
 @EnableRedisRepositories(basePackages = ["org.pofo.domain.redis.domain"])
-class RedisConfig {
-    @Value("\${spring.redis.host}")
-    lateinit var host: String
-
-    @Value("\${spring.redis.port}")
-    lateinit var port: String
-
-    @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(host, port.toInt())
-}
+class RedisConfig
