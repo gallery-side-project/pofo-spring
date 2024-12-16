e18542267deab2436512773ce246e7cee3075fb3
HongSukju <ehensnfl@gmail.com>
2024-12-16 10:57:17 +0900

    feat: add redis env

--- pofo-domain/src/main/kotlin/org/pofo/domain/redis/config/RedisConfig.kt
+++ pofo-domain/src/main/kotlin/org/pofo/domain/redis/config/RedisConfig.kt
@@ -1,8 +1,21 @@
 package org.pofo.domain.redis.config
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @Configuration
 @EnableRedisRepositories(basePackages = ["org.pofo.domain.redis.domain"])
-class RedisConfig
+class RedisConfig {
+    @Value("\${spring.redis.host}")
+    lateinit var host: String
+
+    @Value("\${spring.redis.port}")
+    lateinit var port: String
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(host, port.toInt())
+}
