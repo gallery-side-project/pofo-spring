1181c77c62667eea9e24091859bbe209a4c8d5c7
HongSukju <ehensnfl@gmail.com>
2025-01-22 18:04:22 +0900

    hotfix: unsignned redis port and host

--- src/main/kotlin/org/pofo/api/common/config/RedisConfig.kt
+++ src/main/kotlin/org/pofo/api/common/config/RedisConfig.kt
@@ -1,5 +1,6 @@
 package org.pofo.api.common.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -8,8 +9,14 @@ import org.springframework.data.redis.core.StringRedisTemplate
 
 @Configuration
 class RedisConfig {
+    @Value("\${spring.redis.host}")
+    lateinit var host: String
+
+    @Value("\${spring.redis.port}")
+    var port: Int = 6379
+
     @Bean
-    fun redisConnectionFactory(): LettuceConnectionFactory = LettuceConnectionFactory()
+    fun redisConnectionFactory(): LettuceConnectionFactory = LettuceConnectionFactory(host, port)
 
     @Bean
     fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): StringRedisTemplate {
